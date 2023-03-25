ae4c66ddec7554d930f0f9deedb4006d385d4684
AndreyF <f008f@mai;.ru>
2023-03-25 22:36:51 +0300

    task60 fixed

--- Task60/Program.cs
+++ Task60/Program.cs
@@ -6,18 +6,46 @@
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
-//заполнение массива случайными int от 10 до 99
-void FillArray3D(int [,,] array3D)
+//заполнение одномерного массива случайными неповторяющимися int от 10 до 99
+void FillArray1D(int [] array1D)
 {
     Random random = new Random();
+    int temporaryValue = 0;
+    for (int i = 0; i < array1D.Length; i ++)
+    {
+        if(i > 0)
+        {
+            temporaryValue = random.Next(10, 100);
+            for (int j = 0; j < i; j++)
+                {
+                    while(temporaryValue == array1D[j])
+                    {       
+                        temporaryValue = random.Next(10, 100);
+                        j = 0;
+                        array1D[i] = temporaryValue;
+                    }
+                    array1D[i] = temporaryValue;
+                }
+        }
+        else
+        {
+           array1D[i] = random.Next(10, 100);
+        }
+    }
+}
 
+//заполнение 3D массива числами из массива 1D
+void FillArray3D(int [,,] array3D, int [] array1D)
+{
+int countArray1D = 0;
     for (int i = 0; i < array3D.GetLength(0); i ++)
     {
         for (int j = 0; j < array3D.GetLength(1); j ++)
         {
             for (int k = 0; k < array3D.GetLength(2); k ++)
             {
-                array3D[i, j, k] = random.Next(10, 100);
+                array3D[i, j, k] = array1D[countArray1D];
+                countArray1D++;
             }
         }
     }
@@ -43,8 +71,11 @@ void PrintArray3D(int [,,] array3D)
 int rowsArray3D = 2;
 int colonsArray3D = 2;
 int depthArray3D = 2;
+int lengthArray1D = rowsArray3D*colonsArray3D*depthArray3D;
 
+int[] array1D = new int[lengthArray1D];
 int[,,] array3D = new int[rowsArray3D, colonsArray3D, depthArray3D];
-FillArray3D(array3D);
+FillArray1D(array1D);
+FillArray3D(array3D, array1D);
 PrintArray3D(array3D);
 
